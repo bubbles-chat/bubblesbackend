ea4c2bea7bad1f8c8875652657cdab9ec2558767
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-18 17:26:58 +0300

    feat: add accept request controller function

--- src/modules/request/request.controller.ts
+++ src/modules/request/request.controller.ts
@@ -3,6 +3,7 @@ import ConnectionRequest from "../../models/Request.model";
 import { notifyUser } from "../../services/fcm.service";
 import { Types } from "mongoose";
 import User from "../../models/User.model";
+import Chat from "../../models/Chat.model";
 
 export const addRequest = async (req: Request, res: Response): Promise<void> => {
     const { receiver } = req.params
@@ -74,7 +75,7 @@ export const rejectRequest = async (req: Request, res: Response): Promise<void> 
 
     const request = await ConnectionRequest.findById(id)
 
-    if (request?.receiver.equals(req.authUser._id)) {
+    if (!request?.receiver.equals(req.authUser._id)) {
         res.status(400).json({ message: "You can't reject this request. You are not the receiver" })
         return
     }
@@ -103,4 +104,55 @@ export const getPendingRequestForAUser = async (req: Request, res: Response): Pr
     const requests = await ConnectionRequest.find({ receiver: _id, status: 'pending' }).populate(['receiver', 'sender'])
 
     res.status(200).json({ message: "Requests found successfully", requests })
+}
+
+export const acceptReuqest = async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params
+
+    if (!id) {
+        res.status(400).json({ message: "No request id was provided" })
+        return
+    }
+
+    const request = await ConnectionRequest.findById(id)
+
+    if (!request?.receiver.equals(req.authUser._id)) {
+        res.status(400).json({ message: "You can't accept this request. You are not the receiver" })
+        return
+    }
+
+    const acceptedRequest = await ConnectionRequest.findByIdAndUpdate(id, {
+        status: 'accepted'
+    }, { new: true })
+
+    if (!acceptedRequest) {
+        res.status(404).json({ message: "Reuqest not found" })
+        return
+    }
+
+    const chat = await Chat.create({
+        participants: [acceptedRequest.sender, acceptedRequest.receiver]
+    })
+
+    await User.findByIdAndUpdate(acceptedRequest.sender, {
+        $push: {
+            connections: acceptedRequest.receiver,
+            chats: {
+                $each: [chat._id],
+                $position: 0
+            }
+        }
+    })
+
+    await User.findByIdAndUpdate(acceptedRequest.receiver, {
+        $push: {
+            connections: acceptedRequest.sender,
+            chats: {
+                $each: [chat._id],
+                $position: 0
+            }
+        }
+    })
+
+    res.status(200).json({ message: 'Request has been accepted', request: acceptedRequest })
 }
