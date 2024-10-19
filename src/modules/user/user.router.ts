b93161aa63628a324ac6c52637f0b4d64ff0da05
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-19 15:09:06 +0300

    feat: add get user by id route

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
-import { addUser, deleteUserByEmail, getUserByEmail, getUserByUsername, updateUserInfo } from "./user.controller";
+import { addUser, deleteUserByEmail, getUserByEmail, getUserById, getUserByUsername, updateUserInfo } from "./user.controller";
 
 const userRouter = Router()
 
 userRouter.post('/addUser', expressAsyncHandler(addUser))
 userRouter.get('/getUserByEmail/:email', expressAsyncHandler(getUserByEmail))
 userRouter.get('/getUserByUsername/:username', expressAsyncHandler(getUserByUsername))
+userRouter.get('/getUserById/:id', expressAsyncHandler(getUserById))
 userRouter.put('/updateUser/:id', expressAsyncHandler(updateUserInfo))
 userRouter.delete('/deleteUser/:email', expressAsyncHandler(deleteUserByEmail))
 
