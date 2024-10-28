1d743831b980da66d515104e939f0b3af940059b
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-10-29 00:48:38 +0300

    feat: add uploadAttachment route

--- src/modules/message/message.route.ts
+++ src/modules/message/message.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
-import { getMessage, getMessages } from "./message.controller";
+import { getMessage, getMessages, uploadAttachment } from "./message.controller";
+import upload from "../../middlewares/multer.middleware";
 
 const messageRouter = Router()
 
 messageRouter.get('/getMessages/:id', expressAsyncHandler(getMessages))
 messageRouter.get('/getMessage/:id', expressAsyncHandler(getMessage))
+messageRouter.post('/uploadAttachment/:chatId', upload.single('file'), uploadAttachment)
 
 export default messageRouter
