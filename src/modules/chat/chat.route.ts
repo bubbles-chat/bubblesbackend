d5e5a1cb3f16681d78b3ad64578fef9554e24758
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-11-10 00:30:56 +0200

    feat: add change group chat photo route

--- src/modules/chat/chat.route.ts
+++ src/modules/chat/chat.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
-import { addUserToGroupChat, changeParticipantRoleToAdmin, createGroupChat, removeParticipant } from "./chat.controller";
+import { addUserToGroupChat, changeGroupChatPhoto, changeParticipantRoleToAdmin, createGroupChat, removeParticipant } from "./chat.controller";
+import upload from "../../middlewares/multer.middleware";
 
 const chatRouter = Router()
 
@@ -8,5 +9,6 @@ chatRouter.post('/createGroupChat', expressAsyncHandler(createGroupChat))
 chatRouter.put('/addUserToGroupChat/:userId/:chatId', expressAsyncHandler(addUserToGroupChat))
 chatRouter.put('/changeParticipantRoleToAdmin/:userId/:chatId', expressAsyncHandler(changeParticipantRoleToAdmin))
 chatRouter.put('/removeParticipant/:userId/:chatId', expressAsyncHandler(removeParticipant))
+chatRouter.put('/changeGroupChatPhoto/:chatId', upload.single('file'), expressAsyncHandler(changeGroupChatPhoto))
 
 export default chatRouter
